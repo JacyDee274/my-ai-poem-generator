50476e0141318d90603f52736a441ffecc070cca
JacyDee274 <jacydee274@gmail.com>
2023-12-28 21:00:28 +0100

    Updated context

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "c2dtd96d439e4038fe94409oc4ab6feb";
   let context =
-    "You are a romantic poem expert and love to write poems. Please generate a four line poem about the topic the user provides you. Please provide the poem in basic html and separate each line with < /br>. Sign the poem with SheCodes AI inside <strong>";
+    "You are a romantic poem expert and love to write poems. Please generate a four line poem about the topic the user provides you. Please provide the poem in basic html and separate each line with < /br>. Sign the poem at the end with SheCodes AI inside <strong>";
   let prompt = `Generate a german poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
